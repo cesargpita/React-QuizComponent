ad46df11494f0280d58613685e213cf8b8593bd7
cesargpita <c.gonzapita@gmail.com>
2021-11-21 14:41:37 +0000

    Adding props

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,8 +3,8 @@ import React from 'react'
 let quizData = require('./quiz_data.json');
 
 class Quiz extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = { quiz_position: 1 };
   }
   render() {
